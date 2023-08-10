eeab4885a66d1eb3742133b31c094d7c2cbe7cf3
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-08-10 22:02:57 +0630

    added skill

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,49 +1,78 @@
+/* eslint-disable react/no-unescaped-entities */
+import { BiSolidRightArrow } from "react-icons/bi";
 const Skill = () => {
   return (
     <div className="pt-24 pr-16 px-3" id="skill">
-      <div className="flex flex-wrap gap-10">
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          HTML
+      <h1 className="text-lg">
+        Here are some of the technologies I've been working on within my side
+        projects:
+      </h1>
+      <div className="flex mt-12 gap-10">
+        <div>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">HTML</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">CSS</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">JavaScript</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Ruby</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Python</span>
+          </h1>
         </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          CSS
+        <div>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">React</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Redux</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Nextjs</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Tailwindcss</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Postgresql</span>
+          </h1>
         </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Tailwindcss
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          JavaScript
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          React
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Redux
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Nodejs
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          ExpressJs
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Ruby
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Python
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Rails
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Django
-        </div>
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Flask
-        </div>
-
-        <div className="border-[1px] px-2 py-1 rounded-md text-gray-400 border-gray-400 hover:-translate-y-2 cursor-pointer duration-500">
-          Postgresql
+        <div>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Nodejs</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Expressjs</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Ruby On Rails</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Django</span>
+          </h1>
+          <h1 className="mb-5 flex items-center gap-2">
+            <BiSolidRightArrow className="text-sm text-[#4FC6C9]" />
+            <span className="text-gray-400">Flask</span>
+          </h1>
         </div>
       </div>
     </div>
