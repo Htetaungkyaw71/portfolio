0656989c7e8142eb78ced159f959768b9ce3d4ba
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-08-12 15:33:39 +0630

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import SideContext from "./components/SideContext";
+import Loader from "./components/Loader";
 
 function App() {
   const [activeTag, setActiveTag] = useState("about");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   // Highlight
   const [highlightPosition, setHighlightPosition] = useState({ x: 0, y: 0 });
 
@@ -19,24 +28,30 @@ function App() {
   // Highlight
 
   return (
-    <div
-      className="bg-slate-900 min-h-screen text-white overflow-hidden"
-      id="container"
-      onMouseMove={handleMouseMove}
-    >
-      <div
-        className="highlight"
-        style={{ left: highlightPosition.x, top: highlightPosition.y }}
-      ></div>
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 ">
-        <div className="mt-5 p-5 sm:p-5 md:p-5 lg:p-16 xl:p-16">
-          <Header activeTag={activeTag} setActiveTag={setActiveTag} />
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div
+          className="bg-slate-900 min-h-screen text-white overflow-hidden"
+          id="container"
+          onMouseMove={handleMouseMove}
+        >
+          <div
+            className="highlight"
+            style={{ left: highlightPosition.x, top: highlightPosition.y }}
+          ></div>
+          <div className="container mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 ">
+            <div className="mt-5 p-5 sm:p-5 md:p-5 lg:p-16 xl:p-16">
+              <Header activeTag={activeTag} setActiveTag={setActiveTag} />
+            </div>
+            <div className="overflow-y-auto">
+              <SideContext setActiveTag={setActiveTag} />
+            </div>
+          </div>
         </div>
-        <div className="overflow-y-auto">
-          <SideContext setActiveTag={setActiveTag} />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#1D2C4E]">
+      <span className="loader">
+        <span className="mx-1">Htet</span>
+      </span>
+      ;
+    </div>
+  );
+};
+
+export default Loader;
