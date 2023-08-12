efd1ac76a43bba57b69042f5c21d4fbb440cb388
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-08-12 21:02:28 +0630

    fix: loader size

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-[#1D2C4E]">
       <span className="loader">
-        <span className="mx-1">Htet</span>
+        <span className="mx-1 text-3xl">Htet</span>
       </span>
       ;
     </div>
